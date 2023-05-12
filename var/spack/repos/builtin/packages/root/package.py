2ae0696639690464fb0252b14d05fe742308a735
Chris Green <greenc@fnal.gov>
2023-05-12 13:47:00 -0500

    Add X-related build dependencies

--- var/spack/repos/builtin/packages/root/package.py
+++ var/spack/repos/builtin/packages/root/package.py
@@ -232,6 +232,9 @@ class Root(CMakePackage):
     depends_on("libxft", when="+x")
     depends_on("libxpm", when="+x")
     depends_on("libsm", when="+x")
+    depends_on("fontconfig", when="+x", type="build")
+    depends_on("xproto", when="+x", type="build")
+    depends_on("xextproto", when="+x", type="build")
 
     # OpenGL
     depends_on("ftgl@2.4.0:", when="+opengl")
