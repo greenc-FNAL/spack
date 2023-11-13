db7bfc99d4f38072e732fb79ee82999250f6fa3e
Marc Mengel <mengel@fnal.gov>
2023-11-13 17:16:43 -0600

    tidbits missed from 0.21.0-dev.fermi

--- var/spack/repos/builtin/packages/protobuf/package.py
+++ var/spack/repos/builtin/packages/protobuf/package.py
@@ -37,6 +37,7 @@ class Protobuf(CMakePackage):
     version("3.17.3", sha256="c6003e1d2e7fefa78a3039f19f383b4f3a61e81be8c19356f85b6461998ad3db")
     version("3.17.0", sha256="eaba1dd133ac5167e8b08bc3268b2d33c6e9f2dcb14ec0f97f3d3eed9b395863")
     version("3.16.0", sha256="7892a35d979304a404400a101c46ce90e85ec9e2a766a86041bb361f626247f5")
+    version("3.15.8", sha256="0cbdc9adda01f6d2facc65a22a2be5cecefbefe5a09e5382ee8879b522c04441")
     version("3.15.7", sha256="efdd6b932a2c0a88a90c4c80f88e4b2e1bf031e7514dbb5a5db5d0bf4f295504")
     version("3.15.5", sha256="bc3dbf1f09dba1b2eb3f2f70352ee97b9049066c9040ce0c9b67fb3294e91e4b")
     version("3.15.4", sha256="07f8a02afc14a657f727ed89a8ec5627b9ecc47116d60acaabaa1da233bd2e8f")
