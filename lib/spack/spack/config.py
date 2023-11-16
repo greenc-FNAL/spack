eee073a46165e58b8e33ff96293a81a7e9b12c53
Chris Green <greenc@fnal.gov>
2023-11-16 14:15:05 -0600

    Revert "New configuration scope for OS (_e.g._ `scientific7`)"
    This reverts commit d75a9e7f18cdefedd6e29a753bdfa14222421ae9.

--- lib/spack/spack/config.py
+++ lib/spack/spack/config.py
@@ -754,15 +754,6 @@ def _add_platform_scope(cfg, scope_type, name, path):
     cfg.push_scope(scope_type(plat_name, plat_path))
 
 
-def _add_os_scope(cfg, scope_type, name, path):
-    """Add an os-specific subdirectory for the current platform."""
-    host_platform = spack.platforms.host()
-    oss = str(host_platform.operating_system("frontend"))
-    os_name = os.path.join(name, oss)
-    os_path = os.path.join(path, oss)
-    cfg.push_scope(scope_type(os_name, os_path))
-
-
 def _add_command_line_scopes(cfg, command_line_scopes):
     """Add additional scopes from the --config-scope argument.
 
@@ -780,7 +771,6 @@ def _add_command_line_scopes(cfg, command_line_scopes):
         name = "cmd_scope_%d" % i
         cfg.push_scope(ImmutableConfigScope(name, path))
         _add_platform_scope(cfg, ImmutableConfigScope, name, path)
-        _add_os_scope(cfg, ImmutableConfigScope, name, path)
 
 
 def create():
@@ -829,7 +819,6 @@ def create():
 
         # Each scope can have per-platfom overrides in subdirectories
         _add_platform_scope(cfg, ConfigScope, name, path)
-        _add_os_scope(cfg, ConfigScope, name, path)
 
     # add command-line scopes
     _add_command_line_scopes(cfg, COMMAND_LINE_SCOPES)
