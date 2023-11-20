97e18ec1bfc836e00fb2d7e29fc525a017fe4729
Chris Green <greenc@fnal.gov>
2023-11-20 11:42:55 -0600

    [glib] Invoke `fix_python_path()` at build-system-specific phases

--- var/spack/repos/builtin/packages/glib/package.py
+++ var/spack/repos/builtin/packages/glib/package.py
@@ -211,7 +211,6 @@ class BaseBuilder(metaclass=spack.builder.PhaseCallbacksMeta):
     def dtrace_copy_path(self):
         return join_path(self.stage.source_path, "dtrace-copy")
 
-    @run_before("install")
     def fix_python_path(self):
         if not self.spec.satisfies("@2.53.4:"):
             return
@@ -327,6 +326,10 @@ class MesonBuilder(BaseBuilder, spack.build_systems.meson.MesonBuilder):
                     args.append("-Diconv=gnu")
         return args
 
+    @run_before("meson")
+    def fix_python_path(self):
+        super().fix_python_path()
+
 
 class AutotoolsBuilder(BaseBuilder, spack.build_systems.autotools.AutotoolsBuilder):
     def configure_args(self):
@@ -369,3 +372,7 @@ class AutotoolsBuilder(BaseBuilder, spack.build_systems.autotools.AutotoolsBuild
         args.append("GTKDOC_MKPDF={0}".format(true))
         args.append("GTKDOC_REBASE={0}".format(true))
         return args
+
+    @run_before("configure")
+    def fix_python_path(self):
+        super().fix_python_path()
