cd64a4980326233a2d035751195b7973513e9c2d
Patrick Gartung <gartung@fnal.gov>
2024-10-04 11:54:07 -0500

    llvm add level=0 for Z3 patch

--- var/spack/repos/builtin/packages/llvm/package.py
+++ var/spack/repos/builtin/packages/llvm/package.py
@@ -617,7 +617,7 @@ class Llvm(CMakePackage, CudaPackage, LlvmDetection, CompilerPackage):
 
     patch("sanitizer-platform-limits-posix-xdr-macos.patch", when="@10:14 platform=darwin")
 
-    patch("llvm-cmake-modules-FindZ3.patch", when="@14.0.6")
+    patch("llvm-cmake-modules-FindZ3.patch", level=0,  when="@14.0.6")
 
     @when("@14:17")
     def patch(self):
