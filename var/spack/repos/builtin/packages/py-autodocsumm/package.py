76e40744c67651e006a37e3fc24aaaf6e9aaa27d
Chris Green <greenc@fnal.gov>
2025-01-10 09:23:54 -0600

    py-autodocsumm: version/dependency updates

--- var/spack/repos/builtin/packages/py-autodocsumm/package.py
+++ var/spack/repos/builtin/packages/py-autodocsumm/package.py
@@ -15,8 +15,10 @@ class PyAutodocsumm(PythonPackage):
 
     license("Apache-2.0")
 
+    version("0.2.14", sha256="2839a9d4facc3c4eccd306c08695540911042b46eeafcdc3203e6d0bab40bc77")
     version("0.2.11", sha256="183212bd9e9f3b58a96bb21b7958ee4e06224107aa45b2fd894b61b83581b9a9")
 
     depends_on("py-setuptools@61.0:", type="build")
     depends_on("py-versioneer+toml", type="build")
-    depends_on("py-sphinx@2.2:7", type=("build", "run"))
+    depends_on("py-sphinx@2.2:7", when="@0.2.11", type=("build", "run"))
+    depends_on("py-sphinx@4:8", when="@0.2.14:", type=("build", "run"))
